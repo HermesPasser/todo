c1af43b2dfbbd9ab9a4bdf8542ded0e86b7244d1
Douglas Lacerda <dsldouglas@hotmail.com>
2021-09-24 16:03:03 -0300

    completed List/ListItem functionality

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,48 @@
-import { Fragment } from "react/cjs/react.production.min";
 import ListItem from "./ListItem";
 
-export default function List() {
+export default function List({state, setState}) {
+    const items = state.listItems
+    const length = items.length
+    let itemsOnDisplay = []
+
+    if (state.activeFilter === 0) {
+        itemsOnDisplay = items
+    } else if (state.activeFilter === 1) {
+        itemsOnDisplay = items.filter(item => !item.done)
+    } else {
+        itemsOnDisplay = items.filter(item => item.done)
+    }
+    
+    const filterClicked = (id) => setState({...state, activeFilter: id})
+    const clearClicked = () => setState({
+        ...state, 
+        listItems: state.listItems.filter(item => item.done)        
+    })
+
     return (
         <div className="list">
-            <ListItem label="Placeholder" value="1"  />
-            <ListItem label="Placeholder" value="2" checked={true} />
+            <ul>
+                {
+                    itemsOnDisplay.map((item, i) => {
+                        return <ListItem
+                            state={state}
+                            setState={setState}
+                            
+                            label={item.text}
+                            checked={item.done}
+                            value={i}  /> 
+                    })
+                }
+            </ul>
+
             <div className="filter-wrapper">
-                <a href="#">0 items left</a>
+                <a href="#">{length} items left</a>
                 <div className="inner-filter-wrapper">
-                    <a href="#">All</a>
-                    <a href="#">Active</a>
-                    <a href="#">Completed</a>
+                    <a href="#" onClick={() => filterClicked(0)} >All</a>
+                    <a href="#" onClick={() => filterClicked(1)} >Active</a>
+                    <a href="#" onClick={() => filterClicked(2)} >Completed</a>
                 </div>
-                <a href="#">Clear Completed</a>
+                <a href="#" onClick={clearClicked}>Clear Completed</a>
             </div>
         </div>
     )    
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,21 @@
+import { useState } from "react"
 
-export default function ListItem({label, value, checked = false}) {
+export default function ListItem({state, setState, label, value, checked = false}) {
+    const [done, setCompletion] = useState(checked)
+
+    const handleChecked = (event) => {
+        const done_ = event.target.checked
+        let items = [...state.listItems]
+        const index = items.findIndex(item => item.text === label)
+        items[index]['done'] = done_
+
+        setState({...state, listItems: items})
+        setCompletion(done_)
+    }
+    
     return (
         <li className="list-item">
-            <input type="checkbox" value={value} name="radio" checked={checked}  />
+            <input type="checkbox" value={value} checked={done} onChange={handleChecked} />
             <label>{label}</label>
         </li>
     )    
