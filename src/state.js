21c8866aeaa57115270499a480c5662984d89bc6
Douglas Lacerda <dsldouglas@hotmail.com>
2021-09-24 14:47:34 -0300

    created state

---
+++ src/state.js
@@ -0,0 +1,8 @@
+
+const initialState = {
+    listItems: [], // [{ text: '', done: false }, ... ]
+    darkMode: false,
+    activeFilter: 0 // all 0, active 1, completed 2
+}
+
+export default initialState
