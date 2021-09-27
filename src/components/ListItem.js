a616441858036195c1aae9686a30cb00426b691c
Douglas Lacerda <dsldouglas@hotmail.com>
2021-09-27 12:23:57 -0300

    implement remove item button

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -9,10 +9,16 @@ export default function ListItem({state, setState, label, value, checked}) {
         setState({...state, listItems: items})
     }
 
+    const closeClicked = (event) => {
+        let itemsCopy = [...state.listItems].filter(item => item.text !== label)
+        setState({...state, listItems: itemsCopy})
+    }
+
     return (
         <li className="list-item">
             <input type="checkbox" value={value} checked={checked} onChange={handleChecked} />
             <label>{label}</label>
+            <button onClick={closeClicked}>x</button>
         </li>
     )    
 }
